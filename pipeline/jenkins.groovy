cf13a60dff4840a6256460cf84469ba5480a3329
Konstantin Sorokin <konstantin.sorokin@lseg.com>
2023-12-23 21:38:22 +0200

    add pipeline

---
+++ pipeline/jenkins.groovy
@@ -0,0 +1,19 @@
+pipeline {
+    agent any
+    parameters {
+
+        choice(name: 'OS', choices: ['linux', 'darwin', 'windows', 'all'], description: 'Pick OS')
+        choice(name: 'ARCH', choices: ['arm64', 'amd64', 'anm', 's86'], description: 'Pick Arch')
+
+    }
+    stages {
+        stage('Example') {
+            steps {
+                echo "Build for platform ${params.OS}"
+
+                echo "Build for arch: ${params.ARCH}"
+
+            }
+        }
+    }
+}
